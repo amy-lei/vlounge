24876fc831b3264f4575c8b2befbc44402e3823b
wu-rymd <wu.raymond@columbia.edu>
2020-09-19 18:08:48 -0400

    add db ops

--- server/src/app.py
+++ server/src/app.py
@@ -1,3 +1,4 @@
+pl
 import random
 from flask import Flask, request, g
 from flask_socketio import SocketIO, emit
@@ -11,14 +12,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
 
 db = SQLAlchemy(app)
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), unique=True, nullable=False)
-#     is_flagged = db.Column(db.Boolean(), default=False)
-#     has_joined = db.Column(db.Boolean(), default=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    is_flagged = db.Column(db.Boolean(), default=False)
 
-#     def __repr__(self):
-#         return '<User %r>' % self.name
+    def set_is_flagged(self, new_state):
+        self.is_flagged = new_state
+
+    def __repr__(self):
+        return '<User %r>' % self.name
 
 
 socketIo = SocketIO(app, cors_allowed_origins="*")
@@ -43,12 +46,21 @@ def addUser(name):
     USERS[name] = new_user
     # tell others to update thier list
     emit("updateUser", new_user.json_rep(), broadcast=True)
+    # add user to database
+    db.session.add(new_user)
+    db.session.commit()
     print("sent updateUser")
 
 @socketIo.on("toggleFlag")
 def toggleFlag(name):
     user = USERS[name]
-    user.is_flagged = not user.is_flagged
+    # update flag status in database
+    user_in_db = User.query.filter_by(name=name).first()
+    if user_in_db:
+        user_in_db.set_is_flagged(user.is_flagged)
+        db.session.commit()
+    else:
+        raise Exception(f'From app.py:toggleFlag() No user named {user} ')
     # print("---\n" + user.json_rep() + "\n---\n")
     emit("updateUser", user.json_rep(), broadcast=True)
     print("sent updateUser")
