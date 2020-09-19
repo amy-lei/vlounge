71b67b5e3c80dfbde52bd213a0364d13d6364008
wu-rymd <wu.raymond@columbia.edu>
2020-09-19 17:02:54 -0400

    add thing

---
+++ client/src/components/player.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Player() {
+    window.open("https://music.youtube.com/watch?v=6yWBK8Hp4B8&list=PLiV4rmppLa4bsAEhAxBgUF8oc7gtDfp0z");
+    return (
+        <div></div>
+    )
+}
+
+export default Player;
